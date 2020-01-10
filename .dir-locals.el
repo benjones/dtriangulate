dd195bb03ec0d99b72bb22e5828449cb1315d894
Ben Jones <ben.james.jones@gmail.com>
2020-01-09 17:40:55 -0700

    setup dub properly

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,9 +1,18 @@
 ((d-mode . (
-	    
 	    (setq c-basic-offset 2)
 	    (setq indent-tabs-mode nil)
 	    (setq tab-width 2)
 	    (defvaralias 'c-basic-offset 'tab-width)
 	    (setq tab-stop-list '(2 4 6 8 10 12 14 16 18 20 22 24 26 28 30 32 34 36 38 40 42))
 	    )
-	 ))
+	 )
+ (js-mode . (
+             (setq c-basic-offset 2)
+             (setq indent-tabs-mode nil)
+             (setq tab-width 2)
+             (defvaralias 'c-basic-offset 'tab-width)
+             (setq tab-stop-list '(2 4 6 8 10 12 14 16 18 20 22 24 26 28 30 32 34 36 38 40 42))
+             (setq js-indent-level 2)
+             )
+          )
+ )
